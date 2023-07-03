576b7770254257e0984c4cd0ac82df39e571222d
wisdomhove <hovewisdom@gmail.com>
2023-07-03 13:19:54 +0200

    Creation of a new local and remote git repository to include both the ElavationSimulation and ElavationSimutionTest projects.

---
+++ ElevatorSimulation/Model/IElevatorMovement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulation.Model
+{
+    // Interface for elevator actions
+    public interface IElevatorMovement
+    {
+        void MoveToFloor(Elevator elevator, int floor);
+    }
+}
